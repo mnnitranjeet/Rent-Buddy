58b7924b2131dff7b79d0868b7dbf11f78cad668
mnnitranjeet <ranjeet.2022is20@mnnit.ac.in>
2024-06-01 18:44:32 +0530

    completed contact page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,8 @@ import Profile from "./Pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
 import CreateListing from "./Pages/CreateListing";
+import UpdateListing from "./Pages/UpdateListing";
+import Listing from "./Pages/Listing";
 
 
 export default function App() {
@@ -17,9 +19,14 @@ export default function App() {
     <Route path="/sign-in" element={<SignIn/>} />
     <Route path="/sign-up" element={<SignUp/>} />
     <Route path="/about" element={<About/>} />
+    <Route path='/listing/:listingId' element={<Listing />} />
     <Route element={<PrivateRoute/>} >
     <Route path='/profile' element={<Profile/>}/>
     <Route path='/create-listing' element={<CreateListing/>}/>
+    <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+    />
 
     </Route>
 
