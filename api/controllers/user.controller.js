709dc4dbbcc425d3ebc268b5412c36e9e089fe15
mnnitranjeet <ranjeet.2022is20@mnnit.ac.in>
2024-06-01 09:51:50 +0530

    create get user listing api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,13 +32,27 @@ export const updateUser = async(req,res,next) =>{
 };
 
 export const deleteUser = async(req,res,next) =>{
-    if(req.user.id !== req.params.id) return next(errorHandler(401,'You can only delete your own account!'))
+    if(req.user.id !== req.params.id) 
+        return next(errorHandler(401,'You can only delete your own account!'));
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.clearCookie('access_token');
+        res.status(200).json('User has been deleted!');
+    } catch (error) {
+        next(error);
+    } 
+};
+
+export const getUserListings = async(req,res,next) => {
+   if(req.user.id === req.params.id){
         try {
-            await User.findByIdAndDelete(req.params.id)
-            res.clearCookie('access_token');
-            res.status(200).json('User has been deleted!');
+            const listings = await Listing.find({userRef:req.params.id});
+            res.status(200).json(listings);
         } catch (error) {
             next(error);
         }
-
-}
+   }else{
+    return next(errorHandler(401,'You can only view your own listings'));
+   }
+    
+};
