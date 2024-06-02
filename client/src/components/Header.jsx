169a700520c28f748df2f73a043a6cf5acf2401f
mnnitranjeet <ranjeet.2022is20@mnnit.ac.in>
2024-06-02 10:37:19 +0530

    update header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,7 +4,27 @@ import { useSelector } from 'react-redux';
 import { useEffect,useState } from 'react';
 export default function Header() {
     const {currentUser} = useSelector((state) => state.user);
+    const [searchTerm,setSearchTerm] = useState('');
     const navigate = useNavigate();
+
+    const handleSubmit = (e) =>{
+      e.preventDefault();
+      const urlParams = new URLSearchParams(window.location.search);
+      urlParams.set('searchTerm',searchTerm);
+      const searchQuery = urlParams.toString();
+      navigate(`/search?${searchQuery}`);
+    
+    };
+
+    useEffect(()=>{
+      const urlParams = new URLSearchParams(location.search);
+      const searchTermFormUrl = urlParams.get('searchTerm');
+      if(searchTermFormUrl){
+        setSearchTerm(searchTermFormUrl);
+      }
+    },[location.search]);
+    
+    
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -14,12 +34,20 @@ export default function Header() {
                 <span className='text-slate-700'>Buddy</span>
             </h1>
            </Link>
-            <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
-                <input 
+            <form onSubmit={handleSubmit} className='bg-slate-100 p-3 rounded-lg flex items-center'>
+              <input 
                 type="text" 
                 placeholder="Search..."
-                className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+                className='bg-transparent focus:outline-none w-24 sm:w-64'
+                value={searchTerm}
+                onChange={(e)=>setSearchTerm(e.target.value)}
+              />
+
+              <button>
                 <FaSearch className='text-slate-600'/>  
+              </button>
+
+              
             </form>
 
             <ul className='flex gap-4'>
