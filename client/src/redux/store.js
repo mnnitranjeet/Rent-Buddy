b169550d65669b980cd0543a541f10bb042c4653
mnnitranjeet <ranjeet.2022is20@mnnit.ac.in>
2024-06-02 13:00:25 +0530

    update listing page

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,30 +1,24 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import  userReducer  from './user/userSlice';
-import {persistReducer,persistStore} from 'redux-persist';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { version } from 'mongoose';
-//import persistStore from 'redux-persist/es/persistStore';
 
-
-const rootReducer = combineReducers({user:userReducer})
-
+const rootReducer = combineReducers({ user: userReducer });
 
 const persistConfig = {
-    key:'root',
-    storage,
-    version:1,
+  key: 'root',
+  storage,
+  version: 1,
+};
 
-}
-
-const persistedReducer = persistReducer(persistConfig,rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-  middleware:(getDefaultMiddleware) =>
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-        serializableCheck:false,
-
-  }),
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
