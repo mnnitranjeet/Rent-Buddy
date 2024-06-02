5eb047962d33c111f98fce99693274cbd76bab21
mnnitranjeet <ranjeet.2022is20@mnnit.ac.in>
2024-06-02 13:58:44 +0530

    update api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
     <Route path="/sign-up" element={<SignUp/>} />
     <Route path="/about" element={<About/>} />
     <Route path="/search" element={<Search/>}/>
-    <Route path='/listing/:listingRouteId' element={<Listing />} />
+    <Route path='/listing/:listingId' element={<Listing />} />
 
     
     <Route element={<PrivateRoute/>} >
